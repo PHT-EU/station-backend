4e18298bac376d75cec9d4f8947487f65eecbc20
Michael Graf <michael.graf3110@gmail.com>
2022-05-31 16:28:25 +0200

    moved data set input validation to schema not model

--- station/app/models/datasets.py
+++ station/app/models/datasets.py
@@ -9,37 +9,16 @@ from datetime import datetime
 from station.app.db.base_class import Base
 
 
-class StorageType(Enum):
-    """
-    Enum for storage types
-    """
-    LOCAL = "local"
-    MINIO = "minio"
-    DB = "db"
-
-
-class DataType(Enum):
-    """
-    Enum for data types
-    """
-    IMAGE = "image"
-    GENOME = "genome"
-    FHIR = "fhir"
-    CSV = "csv"
-    STRUCTURED = "structured"
-    UNSTRUCTURED = "unstructured"
-    HYBRID = "hybrid"
-
 
 class DataSet(Base):
     __tablename__ = "datasets"
     id = Column(Integer, primary_key=True, index=True)
     created_at = Column(DateTime, default=datetime.now())
     updated_at = Column(DateTime, nullable=True)
-    proposal_id = Column(Integer, nullable=True)
+    proposal_id = Column(String, nullable=True)
     name = Column(String)
-    data_type = Column(String, default=DataType.FHIR.value)
-    storage_type = Column(String, default=StorageType.MINIO.value)
+    data_type = Column(String, nullable=True)
+    storage_type = Column(String, nullable=True)
     access_path = Column(String, nullable=True)
     fhir_server = Column(Integer, ForeignKey('fhir_servers.id'), nullable=True)
     summary = Column(JSON, nullable=True)
--- station/app/schemas/datasets.py
+++ station/app/schemas/datasets.py
@@ -1,14 +1,41 @@
-from pydantic import BaseModel
 from datetime import datetime
 from typing import Optional, Any
+from enum import Enum
+
+from pydantic import BaseModel
+
+
+class StorageType(Enum):
+    """
+    Enum for storage types
+    """
+    LOCAL = "local"
+    MINIO = "minio"
+    DB = "db"
+
+
+class DataType(Enum):
+    """
+    Enum for data types
+    """
+    IMAGE = "image"
+    GENOME = "genome"
+    FHIR = "fhir"
+    CSV = "csv"
+    STRUCTURED = "structured"
+    UNSTRUCTURED = "unstructured"
+    HYBRID = "hybrid"
 
 
 class DataSetBase(BaseModel):
     name: str
-    data_type: str
-    storage_type: str
+    data_type: Optional[DataType] = None
+    storage_type: Optional[StorageType] = None
     proposal_id: Optional[str] = None
     access_path: Optional[str] = None
+
+    class Config:
+        use_enum_values = True
 
 
 class DataSetCreate(DataSetBase):
