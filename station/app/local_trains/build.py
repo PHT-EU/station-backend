1fbd5547f3b37e87ab187113fb174988355b922b
Michael Graf <michael.graf3110@gmail.com>
2022-08-08 17:36:18 +0200

    local train build

--- station/app/local_trains/build.py
+++ station/app/local_trains/build.py
@@ -1,34 +1,50 @@
 import os
+from io import BytesIO
 from typing import Union, List
 import docker
 from docker.models.images import Image
+from train_lib.docker_util.docker_ops import add_archive
 
 from station.app.local_trains.docker import make_docker_file
 from station.clients.minio import MinioClient
 from station.app.models.local_trains import LocalTrain
+from loguru import logger
 
 
 def build_train(
         train_id: str,
         master_image_id: str = None,
+        files: BytesIO = None,
         entrypoint_file: str = None,
         custom_image: str = None,
-        fhir_query: Union[str, dict] = None) -> Image:
-    minio_client = MinioClient()
+        command: str = None,
+        command_args: List[str] = None
+) -> str:
+    if master_image_id and not files:
+        raise ValueError("Must specify files with master image")
+    if files and not entrypoint_file:
+        raise ValueError("Must specify entrypoint file with files")
 
-    files = minio_client.get_local_train_archive(train_id)
+    image = _make_train_image(
+        train_id=train_id,
+        master_image=master_image_id,
+        entrypoint_file=entrypoint_file,
+        custom_image=custom_image,
+        command=command,
+        command_args=command_args
+    )
 
-    image = _make_train_image(train_id, files, master_image_id, entrypoint_file, custom_image)
+    image = _add_train_files(files=files, image=image)
+
     return image
 
 
 def _make_train_image(
         train_id: str,
-        files: list,
         master_image: str = None,
         entrypoint_file: str = None,
-        command: str = None,
         custom_image: str = None,
+        command: str = None,
         command_args: List[str] = None) -> Image:
     docker_client = docker.from_env()
     if not entrypoint_file and not custom_image:
@@ -36,17 +52,22 @@ def _make_train_image(
     if custom_image:
         image = docker_client.images.get(custom_image)
     elif master_image:
-        image = master_image
-        dockerfile = make_docker_file(image, entrypoint_file, command=command, command_args=command_args)
-        image = docker_client.images.build(fileobj=dockerfile)
+        dockerfile = make_docker_file(master_image, entrypoint_file, command=command, command_args=command_args)
+        image, logs = docker_client.images.build(fileobj=dockerfile)
+        logger.debug(logs)
     else:
         raise ValueError("Must specify an entrypoint file with master image or a custom image")
-    image.tag(repository=train_id, tag="latest")
-    print(image)
+
+    if image.tag(repository=f"pht-local/{train_id}", tag="latest"):
+        logger.debug(f"Tagged image {image.id} as pht-local/{train_id}:latest")
+    else:
+        raise ValueError("Failed to tag image")
     return image
 
 
-def _add_train_files(docker_client: docker.DockerClient, files: list, image: str):
-    container = docker_client.containers.run(image, detach=True)
-    # todo make tarfile
-    # todo add tarfile to container and commit to image
+def _add_train_files(files: BytesIO, image: Image) -> str:
+    # add archive to container
+    tag = image.tags[0]
+    add_archive(tag, files, path="/opt/train")
+
+    return tag
--- station/app/tests/test_local_trains.py
+++ station/app/tests/test_local_trains.py
@@ -1,5 +1,26 @@
+import time
+
+import pytest
+
 from station.app.local_trains.docker import make_docker_file
 from station.app.local_trains.build import build_train
+from io import BytesIO
+import tarfile
+
+
+@pytest.fixture
+def simple_file_archive() -> BytesIO:
+    archive = BytesIO()
+    with tarfile.TarFile(fileobj=archive, mode="w") as tar:
+        content = "import os\n\nif __name__ == '__main__':\n    print('hello')\n"
+        file = BytesIO(content.encode("utf-8"))
+        info = tarfile.TarInfo(name="entrypoint.py")
+        info.size = len(file.getbuffer())
+        info.mtime = time.time()
+        tar.addfile(info, file)
+
+    archive.seek(0)
+    return archive
 
 
 def test_make_docker_file():
@@ -16,5 +37,16 @@ def test_make_docker_file():
     print(docker_file.read())
 
 
-def test_build():
-    pass
+def test_build_basic(simple_file_archive):
+
+    train = build_train(
+        train_id="test",
+        master_image_id="python:3.8",
+        files=simple_file_archive,
+        command="python",
+        entrypoint_file="entrypoint.py",
+    )
+
+    print(train)
+
+
